09f74be5a4181257c853e02c8b0f2f7f9722451a
Brandon Phillips <bmp0015@mix.wvu.edu>
2018-04-29 18:11:00 -0400

    Only install homebrew if it's not already

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # install ansible
 brew install ansible
